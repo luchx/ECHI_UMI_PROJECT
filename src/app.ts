ffbce72cde05fea255301664ac13c2e0734363d7
luchuangxu <lu.chuang.xu@wowoohr.com>
2019-07-07 20:02:21 +0800

    添加@typescript-eslint/parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-
 export const dva = {
   config: {
-    onError     (err: ErrorEvent) {
-            err.preventDefault();
+    onError(err: ErrorEvent) {
+      err.preventDefault();
       console.error(err.message);
     },
   },
